e2aac609a5c960c9c8a5a2b6fd9cb7b4c4b46465
SoniyaMuhammadDawood <164737530+SoniyaMuhammadDawood@users.noreply.github.com>
2024-04-20 20:17:54 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = "true";
+while (condition) {
+    let todosQuestion = await inquirer.prompt([
+        {
+            name: "Question1",
+            type: "input",
+            message: "What would you like to add in your todos",
+        },
+        {
+            name: "Question2",
+            type: "confirm",
+            message: "would you like to add more in your todos",
+            default: "false",
+        }
+    ]);
+    todos.push(todosQuestion.Question1);
+    console.log(todos);
+    condition = todosQuestion.Question2;
+}
